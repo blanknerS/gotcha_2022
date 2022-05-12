8486f2d94ae08dd75cd1f3c4fcba3a37c60bd69a
blankner <blake_ankner23@milton.edu>
2022-05-12 12:06:31 -0400

    Start over

---
+++ Gotcha/Gotcha/GotchaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GotchaApp.swift
+//  Gotcha
+//
+//  Created by Blake Ankner on 5/12/22.
+//
+
+import SwiftUI
+
+@main
+struct GotchaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
