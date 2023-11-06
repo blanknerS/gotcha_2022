10535e6ec8a1c8c5efb69cc9f5094007ed4eb2d4
Bryan6734 <97413577+Bryan6734@users.noreply.github.com>
2023-11-06 17:05:28 -0500

    util functions

--- Website/src/App.js
+++ Website/src/App.js
@@ -1,31 +1,96 @@
 import logo from "./logo.svg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { auth, googleProvider } from "./config/firebase";
-import { signInWithPopup } from "firebase/auth";
+import { onAuthStateChanged } from "firebase/auth";
+import {
+  addTestUser,
+  fetchUserDoc,
+  submitLastWords,
+  tagSelfOut,
+} from "./config/utils";
+import { signInWithPopup, signOut } from "firebase/auth";
 
 import "./App.css";
 
 function App() {
-   const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
+  const [lastWords, setLastWords] = useState("");
 
-    const signInWithGoogle = async () => {
-      try {
-        const res = await signInWithPopup(auth, googleProvider);
-        const user = res.user;
-        setUser(user);
-      } catch (error) {
-        console.log(error.message);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const { userData } = await fetchUserDoc();
+
+        setUser(userData);
+      } else {
+        setUser(null);
       }
-    }
+    });
+  }, [auth.currentUser]);
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setLastWords("");
+    submitLastWords(lastWords);
+  };
+
+  const signInWithGoogle = async () => {
+    try {
+      const res = await signInWithPopup(auth, googleProvider);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSignOut = async () => {
+    try {
+      await signOut(auth);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Gotcha</h1>
         <button onClick={signInWithGoogle}>Sign in with Google</button>
-      </header>
+        <button
+          onClick={() =>
+            addTestUser(
+              "Bryan",
+              "Sukidi",
+              "bryan_sukidi24@milton.edu",
+              "senior",
+              "Boarder",
+              "benjamin_rhodes-kropf24@milton.edu",
+              "katherine_risden24@milton.edu"
+            )
+          }
+        >
+          Test User Add
+        </button>
+        <button onClick={tagSelfOut}>Tag Self Out</button>
+        <button onClick={handleSignOut}>Sign Out</button>
+        <form
+          onSubmit={() => {
+            submitLastWords();
+          }}
+        >
+          <label>Input your last words</label>
+          <input
+            type="text"
+            value={lastWords}
+            onChange={(e) => setLastWords(e.target.value)}
+          />
+          <button type="submit">Submit</button>
+        </form>
 
+        <button onClick={() => console.log(user)}>print user</button>
+
+        <p>{user?.firstName}</p>
+        <p>{user?.lastName}</p>
+      </header>
     </div>
   );
 }
--- Website/src/config/utils.js
+++ Website/src/config/utils.js
@@ -3,14 +3,109 @@
 import { auth, db } from "./firebase";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 
+// Fetch the authenticated user's document from the database
+export const fetchUserDoc = async () => {
+  try {
+
+    // If firebase auth is not initialized, return
+    if (!auth.currentUser) {
+      return;
+    }
+
+    // Get the user's document from the database
+    const userRef = doc(db, "data", auth.currentUser.email);
+    const userDoc = await getDoc(userRef);
+    const userData = userDoc.data();
+
+    // Return ref, doc, and data 
+    return { userRef, userDoc, userData };
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+};
+
+// Fetch a user's document from the database by email (for chasers and targets)
+export const fetchUserDocByEmail = async (email) => {
+  try {
+    const userRef = doc(db, "data", email);
+    const userDoc = await getDoc(userRef);
+    const userData = userDoc.data();
+    return { userRef, userDoc, userData };
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
+// add test user based off params (will remove)
+export const addTestUser = async (
+  firstName,
+  lastName,
+  email,
+  year,
+  livingStatus,
+  chaser,
+  target
+) => {
+  try {
+    const userData = {
+      firstName: firstName,
+      lastName: lastName,
+      class: year,
+      "d/b": livingStatus,
+      chaser: chaser,
+      target: target,
+      alive: true,
+      tags: 0,
+    };
+
+    const userRef = doc(db, "data", email);
+    await setDoc(userRef, userData);
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
+// Tag the current authenticated user out
 export const tagSelfOut = async () => {
+  try {
+    // Get the current user
+    const { userRef, userData } = await fetchUserDoc();
+  
+    // If the user is already out, return
+    if (userData.alive === false) {
+      alert("You are already out!");
+      return;
+    }
 
-  // obtain firebase user and reference to the user document
-  const user = auth.currentUser;
-  const userRef = doc(db, "data", user.uid);
+    // Get the chaser 
+    const { chaserRef, chaserData } = await fetchUserDocByEmail(
+      userData.chaser
+    );
 
-  // obtain user document from reference
-  const userDoc = await getDoc(userRef);
-  const userData = userDoc.data();
+    // Update necessary fields
+    userData.alive = false;
+    chaserData.tags += 1;
+    chaserData.target = userData.target;
 
-}
+    // Post changes to database
+    await setDoc(userRef, userData);
+    await setDoc(chaserRef, chaserData);
+
+  } catch (error) {
+    alert(error.message);
+    console.log(error);
+  }
+};
+
+// Submit last words
+export const submitLastWords = async (lastWords) => {
+  try {
+    const { userRef, userDoc, userData } = await fetchUserDoc();
+    userData.lastWords = lastWords;
+    await setDoc(userRef, userData);
+  } catch (error) {
+    alert(error.message);
+    console.log(error);
+  }
+};
